ee464ad52e451689a6055793e85ff6a7d8cbc494
hubertjb <hubert.jbanville@gmail.com>
2017-05-06 17:38:01 -0400

    - Implementing the predictProba() method - Adding complete example in main()

--- GaussianNaiveBayesClassifier.java
+++ GaussianNaiveBayesClassifier.java
@@ -1,18 +1,13 @@
 import java.util.Arrays;
-import org.apache.commons.lang3.ArrayUtils;
 import java.lang.Math;
 import java.util.*;
 import java.util.stream.*;
+import org.apache.commons.lang3.ArrayUtils;
 
-
-// TODO:
-// 1. Implement predictProba
-// 2. Implement decision boundary method
-//
 
 public class GaussianNaiveBayesClassifier {
 
-	boolean fitted;
+	private boolean fitted;
 
 	private int[] classes;
 	private int nbClasses;
@@ -26,121 +21,185 @@ public class GaussianNaiveBayesClassifier {
 	private double[] classPriors;
 
 	public GaussianNaiveBayesClassifier() {
-		// Gaussian Naive Bayes classifier
+		// Gaussian Naive Bayes classifier.
+		//
+		// A Gaussian Naive Bayes classifier that can be trained offline or 
+		// online, and then used for prediction. It accepts any number of 
+		// classes and features.
+		// The API was inspired by sklearn's:
+		// http://scikit-learn.org/stabl/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB
 		//
 		// Attributes:
-		// 	fitted (bool): True if the model has been fitted
-		// 	classes (int[]): int identifier for each class
-		//  nbClasses (int): number of classes for which the model
-		//  	has been trained
-		// 	nbFeats (int): number of features
-		//  classCounts (int[]): number of examples by class seen
-		//  CONTINUE DESCRIPTION HERE!
+		// 	fitted: True if the model has been fitted
+		// 	classes: list of the integer identifier of each class for which the
+		// 		model has been trained
+		//  nbClasses: number of classes for which the model has been trained
+		// 	nbFeats: number of features
+		//  classCounts: number of examples seen by class
+		//  sum: used internally to update the Gaussian models (sum of all seen
+		//  	examples)
+		//  sumSquares: used internally to update the Gaussian models (sum of 
+		//		squares of all seen examples)
+		//	theta: means of the Gaussian models [nbClasses x nbFeats]
+		//  sigma: variances of the Gaussian models [nbClasses x nbFeats]
+		//  classPriors: prior probability of each class, computed using the 
+		//		number of examples seen for each class during training
 		//
+		// TODO:
+		//  Implement decision boundary method?
 
 		fitted = false;
 	}
 
 	public void fit(double[][] X, int[] y) {
-		// Fit a Gaussian Naive Bayes model
+		// Fit the model.
 		//
 		// Args:
-		// 	X: training data, [nb examples, nb features]
-		//  y: labels [nb examples]
+		// 	X: training data, [nbExamples, nbFeatures]
+		//  y: labels [nbExamples]
   		//
-  		// Note: Calling `fit()` overwrites previous information. Use `partial_fit()`
- 		//  to update the model with new training data.
+  		// Note: Calling `fit()` overwrites previous information. Use 
+  		// `partial_fit()` to update the model with new training data.
 
-		fitted = false; // if model has already been trained, re-initialize parameters
+		// If model has already been trained, re-initialize parameters
+		this.fitted = false;
 		partialFit(X,y);
 	}
 
 	public void partialFit(double[][] X, int[] y) {
-		// Fit or update the GNB model
+		// Fit or update the model.
+		//
+		// Fit or update the model. If the model has already been trained, this
+		// will update the parameters instead of computing them from scratch.
 		//
 		// Args:
-		// 	X: training data, [nb examples, nb features]
-		//  y: labels [nb examples]
+		// 	X: training data, [nbExamples, nbFeatures]
+		//  y: labels [nbExamples]
 		//
 		// Using `partialFit()` allows to update the model given new data.
-		assert (X.length == y.length) : "X and y must contain the same number of examples.";
 
-		if (!fitted) { // model has not been trained yet, initialize parameters
-			classes = unique(y);
-			nbClasses = classes.length;
-			nbFeats = X[0].length;
+		assert (X.length == y.length) : 
+			"X and y must contain the same number of examples.";
 
-			classCounts = new int[nbClasses];
-			classPriors = new double[nbClasses];
-			sum = new double[nbClasses][nbFeats];
-			sumSquares = new double[nbClasses][nbFeats];
-			theta = new double[nbClasses][nbFeats];
-			sigma = new double[nbClasses][nbFeats];
+		// If model has not been trained yet, initialize parameters
+		if (!this.fitted) {
+			this.classes = unique(y);
+			this.nbClasses = this.classes.length;
+			this.nbFeats = X[0].length;
+
+			this.classCounts = new int[this.nbClasses];
+			this.classPriors = new double[this.nbClasses];
+			this.sum = new double[this.nbClasses][this.nbFeats];
+			this.sumSquares = new double[this.nbClasses][this.nbFeats];
+			this.theta = new double[this.nbClasses][this.nbFeats];
+			this.sigma = new double[this.nbClasses][this.nbFeats];
 		}
 
-		int[] newClassCounts = count(y,classes);
+		int[] newClassCounts = count(y,this.classes);
 
-		// Class count, sum, theta, sum of squares and sigma can all be computed per class
-		for (int i = 0; i < nbClasses; i++) {
+		// Compute internal variable and model parameters for each class
+		for (int i = 0; i < this.nbClasses; i++) {
 			classCounts[i] += newClassCounts[i];
 
-			for (int k = 0; k < nbFeats; k++) {
+			for (int k = 0; k < this.nbFeats; k++) {
 				// Update sum and sum of squares
 				for (int j = 0; j < X.length; j++) {
-					if (y[j] == classes[i]) {
-						sum[i][k] += X[j][k];
-						sumSquares[i][k] += X[j][k] * X[j][k];
+					if (y[j] == this.classes[i]) {
+						this.sum[i][k] += X[j][k];
+						this.sumSquares[i][k] += X[j][k] * X[j][k];
 					}
 				}
 				// Update theta and sigma 
-				theta[i][k] = sum[i][k] / classCounts[i];
-				sigma[i][k] = sumSquares[i][k] / classCounts[i] - theta[i][k] * theta[i][k];
+				this.theta[i][k] = this.sum[i][k] / this.classCounts[i];
+				this.sigma[i][k] = this.sumSquares[i][k] / this.classCounts[i] 
+								   - this.theta[i][k] * theta[i][k];
 			}
 		}
 
 		// Update class priors
 		int nbExamplesSeen = IntStream.of(classCounts).sum();
-		for (int i = 0; i < nbClasses; i++) {
-			classPriors[i] = (double) classCounts[i]/nbExamplesSeen;
+		for (int i = 0; i < this.nbClasses; i++) {
+			this.classPriors[i] = (double) classCounts[i]/nbExamplesSeen;
 		}
 
-		fitted = true;
+		this.fitted = true;
 	}
 
 	public double[][] predictProba(double[][] X) {
-		// Evaluate the posterior for each class one by one
+		// Compute the posterior probability.
+		//
+		// Compute the posterior probability of data X given the learned model
+		// parameters.
+		//
+		// Args:
+		//  X: data for which to compute the posterior probability, 
+		// 		[nbExamples, nbFeatures]
+		//
+		// Returns:
+		//  posterior probability of each class for each example in X,
+		//		[nbExamples, nbClasses]
+		//
+		// TODO:
+		// - Add option to use classPriors
 
-	// 	double[][] prob = new double[nbClasses][X.length];
-	// 	for (int i = 0; i < nbClasses; i++) {
-	// 		prob[i] = gaussian(X, theta[i], sigma[i]);
-	// 	}
+		double[][] prob = new double[X.length][this.nbClasses];
+		double partition = 0;
 
-	// 	for i in range(self.nb_classes_):
-	// 		np.prod(self._gaussian(X, self.theta_[i,:], self.sigma_[i,:]), axis=1)
- //        proba = np.column_stack([np.prod(self._gaussian(X, self.theta_[i,:], self.sigma_[i,:]), axis=1) for i in range(self.nb_classes_)])
-        
- //        return proba/np.sum(proba, axis=1).reshape(-1,1)
+		for (int i = 0; i < X.length; i++) {
+			partition = 0;
+			for (int j = 0; j < this.nbClasses; j++) {
+				prob[i][j] = 1;
+				// Compute joint pdf of example i
+				for (int k = 0; k < X[i].length; k++) {
+					prob[i][j] *= gaussian(X[i][k], this.theta[j][k], 
+										   this.sigma[j][k]);
+				}
+				partition += prob[i][j]; // Compute the partition function
+			}
+			// Normalize the pdfs so prob sums to 1 for each example
+			for (int j = 0; j < this.nbClasses; j++) {
+				prob[i][j] /= partition; 
+			}
+		}
 
-		return X;
+		return prob;
 	}
 
 	public int[] predict(double[][] X) {
-		// Classify each example in X
+		// Classify examples.
+		//
+		// Classify examples X given the learned model parameters.
+		//
+		// Args:
+		//  X: data to classify, [nbExamples, nbFeatures]
+		//
+		// Returns:
+		//  predicted labels, [nbExamples]
 
 		double[][] prob = predictProba(X);
 		int[] yHat = new int[X.length];
 
 		for (int i = 0; i < X.length; i++) {
-			yHat[i] = classes[argmax(prob[i])];
+			yHat[i] = this.classes[argmax(prob[i])];
 		}
 		
 		return yHat;
 	}
 
-	public double score(double[][] X, double[] y) {
-		// Return the accuracy of the current model on data X, y
+	public double score(double[][] X, int[] y) {
+		// Estimate the accuracy of the current model.
+		//
+		// Estimate the accuracy of the current model on data X, y.
+		//
+		// Args:
+		//  X: data to test on, [nbExamples, nbFeatures]
+		//  y: labels for X [nbExamples]
+		//
+		// Returns:
+		//  accuracy
 
-		assert (X.length == y.length) : "X and y must contain the same number of examples.";
+		assert (X.length == y.length) : 
+			"X and y must contain the same number of examples.";
 
 		int[] yHat = predict(X);
 		int nbGoodDecisions = 0;
@@ -149,14 +208,14 @@ public class GaussianNaiveBayesClassifier {
 				nbGoodDecisions += 1;
 			}
 		}
-        return nbGoodDecisions/y.length;
+        return (double) nbGoodDecisions/y.length;
 	}
 
 	public double[][] getMeans() {
 		return this.theta;
 	}
 
-	public double[][] getStandardDeviations() {
+	public double[][] getVariances() {
 		return this.sigma;
 	}
 
@@ -169,33 +228,81 @@ public class GaussianNaiveBayesClassifier {
 	}
 
 	public void setMeans(double[][] means) {
+		// Set the means of the model.
+		//
+		// Set the means of the model. This makes it possible to load the
+		// parameters of a model that was trained previously. However, since the 
+		// internal state is unknown (sum, sumSquares, classCounts), we can't 
+		// allow partial training.
+
 		this.theta = means;
-		this.fitted = false; // Since the internal state is not known, we can't allow partial training...
+		this.fitted = false; 
 	}
 
-	public void setStandardDeviations(double[][] stds) {
-		this.sigma = stds;
-		this.fitted = false; // Since the internal state is not known, we can't allow partial training...
+	public void setVariances(double[][] vars) {
+		// Set the variances of the model.
+		//
+		// Set the variances of the model. This makes it possible to load the 
+		// parameters of a model that was trained previously. However, since the
+		// internal state is unknown (sum, sumSquares, classCounts), we can't 
+		// allow partial training.
+
+		this.sigma = vars;
+		this.fitted = false; 
 	}
 
 	public void setClassPriors(double[] classPriors) {
+		// Set the class priors of the model.
+		//
+		// Set the class priors of the model. This makes it possible to 
+		// load the parameters of a model that was trained previously. However, 
+		// since the internal state is unknown (sum, sumSquares, classCounts), 
+		// we can't allow partial training.
+
 		this.classPriors = classPriors;
-		this.fitted = false; // Since the internal state is not known, we can't allow partial training...
+		this.fitted = false; 
 	}
 
 	// public double[][] decisionBoundary() {
 	// 	// ...
 	// }
 
-	private double gaussian(double X, double mu, double var) {
-		double x = (X-mu);
-		return Math.exp(-x*x / (2*var)) / Math.sqrt(2 * Math.PI * var);
+	private double gaussian(double x, double mu, double var) {
+		// Compute the Gaussian pdf.
+		//
+		// Compute the univariate Gaussian pdf of mean `mu` and variance `var`
+		//  for point `x`.
+		//
+		// Args:
+		//  x: point for which to evaluate the pdf
+		//  mu: mean of the univariate Gaussian
+		//  var: variance of the univariate Gaussian
+		//
+		// Returns:
+		//  pdf of point `x`
+		
+		x -= mu;
+		return Math.exp(-x*x / (2*var)) / (Math.sqrt(2 * Math.PI * var));
 	}
 
-	private double[][] gaussian(double[][] X, double[] mu, double[] var) {	
+	private double[][] gaussian(double[][] X, double[] mu, double[] var) {
+		// Compute the Gaussian pdf.
+		//
+		// Compute the Gaussian pdfs for multiple points and features at once.
+		// This is a helper function to simplify the use of gaussian() above
+		// on multiple points and features at once. 
+		//
+		// Args:
+		//  X: matrix of shape [nbExamples, nbFeatures]
+		//  mu: means of the univariate Gaussians, [nbFeatures]
+		//  var: variances of the univariate Gaussians, [nbFeatures]
+		//
+		// Returns:
+		//  pdfs of X, [nbExamples, nbFeatures]
+
 		double[][] g = new double[X.length][mu.length];
-		for (int j=0; j<mu.length; j++){
-			for (int i=0; i<X.length; i++){
+		for (int j = 0; j < mu.length; j++){
+			for (int i = 0; i < X.length; i++){
 				g[i][j] = gaussian(X[i][j], mu[j], var[j]);
 			}
 		}	
@@ -203,10 +310,10 @@ public class GaussianNaiveBayesClassifier {
 	}
 
 	private int[] unique(int[] numbers) {
-		// Find unique elements in array
+		// Find unique elements in array.
 		//
 		// Args:
-		// 	x: array for which to find unique values
+		// 	numbers: array for which to find unique values
 		//
 		// Returns:
 		// 	array containing unique values
@@ -230,7 +337,7 @@ public class GaussianNaiveBayesClassifier {
 	}
 
 	private int[] count(int[] numbers, int[] like) {
-		// Count the number of occurences of a specific value in an array
+		// Count the number of occurences of a specific value in an array.
 		//
 		// Args:
 		// 	numbers: list of integers in which to look for 
@@ -239,7 +346,7 @@ public class GaussianNaiveBayesClassifier {
 		// Returns:
 		// 	number of occurences for each desired element
 		//
-		// TODO: Make a more efficient version
+		// TODO: Make a more efficient version?
 
 		int[] counts = new int[like.length];
 		for (int i : like){
@@ -254,7 +361,13 @@ public class GaussianNaiveBayesClassifier {
 	}
 
 	private int argmax(double[] x) {
-		// Return the index of the element with the highest value in x
+		// Return the index of the element with the highest value in x.
+		//
+		// Args:
+		//  x: array
+		//
+		// Returns:
+		//  index of the element with the highest value in x
 
 		double max = x[0];
 		int maxInd = 0;
@@ -267,9 +380,12 @@ public class GaussianNaiveBayesClassifier {
 		return maxInd;
 	}
 
-
 	public void print() {
-		// Print the current state of the model
+		// Print the current state of the model.
+		System.out.println(" ");
+		System.out.println("Summary of GaussianNaiveBayesClassifier");
+		System.out.println("=======================================");
+		System.out.println(" ");
 		System.out.println("Classes: "+Arrays.toString(this.classes));
 		System.out.println("Number of classes: "+this.nbClasses);
 		System.out.println("Number of features: "+this.nbFeats);
@@ -278,31 +394,61 @@ public class GaussianNaiveBayesClassifier {
 		System.out.println("Sums: "+Arrays.deepToString(this.sum));
 		System.out.println("Sums of squares: "+Arrays.deepToString(this.sumSquares));
 		System.out.println("Means: "+Arrays.deepToString(getMeans()));
-		System.out.println("Standard deviations: "+Arrays.deepToString(getStandardDeviations()));
+		System.out.println("Variances: "+Arrays.deepToString(getVariances()));
+		System.out.println(" ");
 	}
 
 	public static void main(String[] args) {
 
 		GaussianNaiveBayesClassifier clf = new GaussianNaiveBayesClassifier();
 
-		// Test unique() method
+		// Make initial training set
+		double[][] X_train = new double[][]{{1, 2},
+									   		{4, 4},
+									  		{4, 6},
+									  		{7, 8},
+									 		{10, 10},
+									  		{30, 30}};
+		int[] y_train = {0, 0, 0, 0, 1, 1};
 
+		// Make second training set for update
+		double[][] X_train2 = new double[][]{{2, 3},
+									   		 {5, 5},
+									  		 {5, 7},
+									  		 {8, 9},
+									 		 {11, 11},
+									  		 {31, 31}};
+		int[] y_train2 = {0, 0, 0, 0, 1, 1};
 
-		double[][] X = new double[][]{{1, 2},
-									  {4, 4},
-									  {4, 6},
-									  {7, 8}};
-		int[] y = {0, 0, 0, 1};
+		// Make test set
+		double[][] X_test = new double[][]{{0, 0},
+										   {4, 5},
+										   {20, 20},
+										   {100, 115}};
+		int[] y_test = {0, 0, 1, 1};
 
-		clf.partialFit(X, y);
-
+		// Fit initial model
+		clf.fit(X_train, y_train);
 		clf.print();
 
-		// Test gaussian() method
-		double[] mu = {4.0, 5.0};
-		double[] var = {0.2, 1.0};
-		double[][] g = clf.gaussian(X, mu, var);
+		// Update model
+		clf.partialFit(X_train2, y_train2);
+		clf.print();
 
-		System.out.println("gaussian: "+Arrays.deepToString(g));
+		// Train model from scratch with new data instead
+		clf.fit(X_train2, y_train2);
+		clf.print();
+
+		// Predict probability
+		double[][] proba = clf.predictProba(X_test);
+		System.out.println("Probability: "+Arrays.deepToString(proba));
+
+		// Predict
+		int[] yHat = clf.predict(X_test);
+		System.out.println("Prediction: "+Arrays.toString(yHat));
+
+		// Score
+		double acc = clf.score(X_test, y_test);
+		System.out.println("Accuracy: "+acc);
 	}
 }
