c8534d111c0f9494ca9d4374655efbb92ac04c72
Raymundo Cassani <raymundo.cassani@gmail.com>
2017-07-18 13:36:21 -0400

    Text-based file for translations, and autogeneration of .js locale files (#32)
    * (A) locales as a text-based CSV file, and (B) python code to create the .js locale files from the CSV file
    
    * Update translation CSV file to contain BCI labels

---
+++ generate_locales.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Generates the locales files for EEG-101, using the information from the CSV file
+'translation_text_file'
+
+@author: Raymundo Cassani raymundo.cassani@gmail.com
+"""
+
+import csv
+import numpy as np
+
+# CSV files with labels for different languages
+translation_text_file = 'translation_text.csv'
+
+with open(translation_text_file, newline='') as csvfile:
+    text_array = np.array(list(csv.reader(csvfile)))
+
+languages = text_array[0 , 1::]
+keys = text_array[1:: , 0]
+
+# create a 'language.js' locale file for each language in the CSV' 
+for language in languages:
+    # find index of a given language
+    language_ix = list(languages).index(language) + 1
+    language_labels = text_array[1:: , language_ix]
+    
+    # create locale file
+    locale_file = open(language + '.js', 'w' , newline='\n')
+    locale_file.write('export default { \n')
+    
+    for key, label in zip(keys, language_labels):
+        if key == label:
+            locale_file.write('\t' + key + '\n')
+        else:
+            locale_file.write('\t' + key + ':  ' + "'" + label + "'" + ',\n')    
+
+    locale_file.write('}')
