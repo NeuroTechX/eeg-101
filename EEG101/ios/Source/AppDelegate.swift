fe8491bbc3615f2c9071d8ee9671b5ac67cd283f
Tom McNeely <tom@tommcneely.ca>
2017-12-16 18:44:39 -0500

    Able to run and see welcome to react native screen

---
+++ EEG101/ios/Source/AppDelegate.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+import React
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index.ios", fallbackResource: nil)
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "EEG101", initialProperties: nil, launchOptions: launchOptions)
+    rootView?.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
